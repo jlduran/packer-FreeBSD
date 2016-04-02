88e96eebdc334b59f615869407d2c1de1fbda2a4
Jose Luis Duran <jlduran@users.noreply.github.com>
2016-04-02 15:01:59 -0300

    Continue execution on freebsd-update error

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@
 ntpdate -v -b in.pool.ntp.org
 
 # Update FreeBSD
-freebsd-update --not-running-from-cron fetch install
+freebsd-update --not-running-from-cron fetch install || true
 
 # Bootstrap pkg
 env ASSUME_ALWAYS_YES=yes pkg bootstrap -yf
