da947acc7872dd780f562533bee613e923ea4749
Jose Luis Duran <jlduran@users.noreply.github.com>
2017-06-30 15:53:50 -0300

    scripts/ansible: one-line pkg install

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -e
 
-# Install python
-pkg install -y python
-
-# Install root certs
-pkg install -y ca_root_nss
+# Install python and root certificates
+pkg install -y python ca_root_nss
