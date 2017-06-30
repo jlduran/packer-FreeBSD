18eb92e4b5bc13cd8093b16d30c1dea1c17f86be
Jose Luis Duran <jlduran@users.noreply.github.com>
2017-06-30 17:49:23 -0300

    scripts/vagrant: minor cleanup

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -7,20 +7,20 @@ echo 'fdescfs		/dev/fd		fdescfs	rw,late	0	0' >> /etc/fstab
 # Create the vagrant user with a password of vagrant
 pw groupadd vagrant -g 1001
 mkdir -p /home/vagrant
-pw useradd vagrant -m -M 0755 -w yes -n vagrant -u 1001 -g 1001 -G 0 \
-	-c 'Vagrant User' -d '/home/vagrant' -s '/usr/local/bin/bash'
+pw useradd -n vagrant -u 1001 -c 'Vagrant User' -d /home/vagrant \
+	-g 1001 -G 0 -m -M 0755 -w yes -s /usr/local/bin/bash
 
 # Configure sudo to allow the vagrant user
 echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /usr/local/etc/sudoers.d/vagrant
-chmod 440 /usr/local/etc/sudoers.d/vagrant
+chmod 0440 /usr/local/etc/sudoers.d/vagrant
 
 # Configure the vagrant ssh key
 mkdir /home/vagrant/.ssh
-chmod 700 /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
 fetch -am --no-verify-peer -o /home/vagrant/.ssh/authorized_keys \
 	'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
 chown -R 1001 /home/vagrant/.ssh
-chmod 600 /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
 
 # Synced folders
 pkg install -y rsync
