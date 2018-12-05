066684a9236b33dc01763eeb30c00ac8c47890f9
Jose Luis Duran <jlduran@users.noreply.github.com>
2018-12-05 11:31:50 -0200

    Do not install X11 for virtualbox
    Use current branch

--- ci/cirrus.sh
+++ ci/cirrus.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 # Prerequisites
-pkg install -y git packer vagrant virtualbox-ose
+pkg install -y git packer vagrant virtualbox-ose-nox11
 
 # Instructions
-git clone https://github.com/jlduran/packer-FreeBSD.git
+git clone --single-branch -b 12.0 https://github.com/jlduran/packer-FreeBSD.git
 cd packer-FreeBSD
 cp variables.json.sample variables.json
 packer build -var-file=variables.json template.json
