3b7703dd908a3f5847e726ae3f7e5019fba18579
Jose Luis Duran <jlduran@users.noreply.github.com>
2018-12-05 11:55:28 -0200

    Fix cryptic error message?

--- ci/cirrus.sh
+++ ci/cirrus.sh
@@ -2,6 +2,12 @@
 
 # Prerequisites
 pkg install -y git packer vagrant virtualbox-ose-nox11
+kldload vboxdrv
+kldload vboxnetflt:ng_vboxnetflt
+kldload vboxnetadp
+pw groupmod vboxusers -m "$(whoami)"
+chown root:vboxusers /dev/vboxnetctl
+chmod 0660 /dev/vboxnetctl
 
 # Instructions
 git clone --single-branch -b 12.0 https://github.com/jlduran/packer-FreeBSD.git
