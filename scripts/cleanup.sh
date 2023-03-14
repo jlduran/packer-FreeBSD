da83e6df0991c68f51d35dc390d3e9f98c2ae849
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-03-14 03:17:30 -0300

    Revert 76b48dc and use the new machine-id file

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -7,6 +7,7 @@ sed -i '' -e 's/^PermitRootLogin yes/#PermitRootLogin no/' /etc/ssh/sshd_config
 # Purge files we no longer need
 rm -rf /boot/kernel.old
 rm -f /etc/hostid
+rm -f /etc/machine-id
 rm -f /etc/ssh/ssh_host_*
 rm -f /root/*.iso
 rm -f /root/.vbox_version
@@ -15,5 +16,4 @@ rm -rf /var/db/freebsd-update/files/*
 rm -f /var/db/freebsd-update/*-rollback
 rm -rf /var/db/freebsd-update/install.*
 rm -f /var/db/pkg/repo-*.sqlite
-rm -f /var/db/machine-id
 rm -rf /var/log/*
