02d6bd182b1af6f30e3e7a7159d901a93c6f4ef4
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-07-19 16:38:02 +0000

    SQUASH: Try passing rc var as an env var

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 set -e
 
-# XXX test
-sysrc ntpdate_hosts="ntp.ubuntu.com"
-# XXX test
-
 # Set the time
 service ntpdate onestart || true
-
-# XXX test
-sysrc ntpdate_hosts=""
-# XXX test
 
 # Update FreeBSD
 freebsd-update --not-running-from-cron fetch install || true
