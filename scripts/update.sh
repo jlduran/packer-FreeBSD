d79048436ebba389d0f4e48c70e6ffc3bbdacfa0
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-08-29 23:07:58 -0300

    Upgrade boot partition (#32)
    * Upgrade boot partition
    * Remove -old.efi backup files

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,3 +12,7 @@ env ASSUME_ALWAYS_YES=yes pkg bootstrap -f
 
 # Upgrade packages
 pkg upgrade -qy
+
+# Upgrade boot partition
+fetch -o /tmp https://raw.githubusercontent.com/freebsd/freebsd-src/main/tools/boot/install-boot.sh
+sh /tmp/install-boot.sh
