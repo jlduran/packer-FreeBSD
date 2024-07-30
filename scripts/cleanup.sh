387db0dfb247b4874020065c67222e54a47b20b9
Jose Luis Duran <jlduran@users.noreply.github.com>
2024-07-30 05:09:00 +0000

    cleanup: Allow rm -f to fail on msdos mounts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,8 +6,6 @@ sed -i '' -e 's/^PermitRootLogin yes/#PermitRootLogin no/' /etc/ssh/sshd_config
 
 # Purge files we no longer need
 rm -rf /boot/kernel.old
-rm -f /boot/efi/EFI/FreeBSD/*-old.efi
-rm -f /boot/efi/EFI/BOOT/*-old.efi
 rm -f /etc/hostid
 rm -f /etc/machine-id
 rm -f /etc/ssh/ssh_host_*
@@ -19,3 +17,9 @@ rm -f /var/db/freebsd-update/*-rollback
 rm -rf /var/db/freebsd-update/install.*
 rm -f /var/db/pkg/repo-*.sqlite
 rm -rf /var/log/*
+
+# /boot/efi is an MS-DOS mount,
+# allow rm -f to fail (EINVAL)
+set +e
+rm -f /boot/efi/EFI/FreeBSD/*-old.efi
+rm -f /boot/efi/EFI/BOOT/*-old.efi
