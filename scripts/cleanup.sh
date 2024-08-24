6992bb081c8eeb210a78a7a9a40bd01d660f8d96
Jose Luis Duran <jlduran@users.noreply.github.com>
2024-08-24 14:21:43 +0000

    Revert "cleanup: Allow rm -f to fail on msdos mounts"
    This reverts commit 387db0dfb247b4874020065c67222e54a47b20b9 and fixes
    it in a different, less explicit way.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,6 +6,9 @@ sed -i '' -e 's/^PermitRootLogin yes/#PermitRootLogin no/' /etc/ssh/sshd_config
 
 # Purge files we no longer need
 rm -rf /boot/kernel.old
+# /boot/efi is an MS-DOS mount, allow rm -f to fail (#281033)
+rm -f /boot/efi/EFI/FreeBSD/*-old.efi || true
+rm -f /boot/efi/EFI/BOOT/*-old.efi || true
 rm -f /etc/hostid
 rm -f /etc/machine-id
 rm -f /etc/ssh/ssh_host_*
@@ -17,9 +20,3 @@ rm -f /var/db/freebsd-update/*-rollback
 rm -rf /var/db/freebsd-update/install.*
 rm -f /var/db/pkg/repo-*.sqlite
 rm -rf /var/log/*
-
-# /boot/efi is an MS-DOS mount,
-# allow rm -f to fail (EINVAL)
-set +e
-rm -f /boot/efi/EFI/FreeBSD/*-old.efi
-rm -f /boot/efi/EFI/BOOT/*-old.efi
